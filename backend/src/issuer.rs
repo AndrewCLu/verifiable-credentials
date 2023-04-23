431fcab76cf6c97a9b08556e2ef344e63ea82a03
AndrewCLu <andrewclu98@gmail.com>
2023-04-22 23:52:36 -0400

    implement http errors returned to user

--- backend/src/issuer.rs
+++ backend/src/issuer.rs
@@ -1,9 +1,37 @@
-use actix_web::{get, post, web, HttpResponse, Responder, Scope};
+use actix_web::{get, post, web, HttpResponse, Responder, ResponseError, Scope};
 use serde::Deserialize;
+use std::fmt;
 use std::sync::Mutex;
 use vc_core::{Issuer, VerificationMethod, URL};
 
 use crate::registry::VerifiableDataRegistry;
+
+#[derive(Debug)]
+pub enum UserError {
+    BadRequest,
+    Unauthorized,
+    NotFound,
+    InternalServerError,
+}
+
+impl ResponseError for UserError {
+    fn error_response(&self) -> HttpResponse {
+        match *self {
+            UserError::BadRequest => HttpResponse::BadRequest().body("Bad Request"),
+            UserError::Unauthorized => HttpResponse::Unauthorized().body("Unauthorized"),
+            UserError::NotFound => HttpResponse::NotFound().body("Resource Not Found"),
+            UserError::InternalServerError => {
+                HttpResponse::InternalServerError().body("Internal Server Error")
+            }
+        }
+    }
+}
+
+impl fmt::Display for UserError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
 
 #[derive(Deserialize)]
 struct CreateIssuerRequest {
@@ -15,14 +43,19 @@ struct CreateIssuerRequest {
 async fn create(
     request: web::Json<CreateIssuerRequest>,
     registry: web::Data<Mutex<VerifiableDataRegistry>>,
-) -> HttpResponse {
-    let mut registry = registry.lock().expect("Could not lock registry.");
-    let issuer_id = URL::new(&request.id).expect("Invalid issuer ID.");
+) -> Result<HttpResponse, UserError> {
+    let mut registry = registry
+        .lock()
+        .map_err(|_e| UserError::InternalServerError)?;
+    let issuer_id = URL::new(&request.id).map_err(|_e| UserError::BadRequest)?;
     let issuer = Issuer::new(issuer_id.clone(), request.name.clone());
 
-    registry.add_issuer(issuer).expect("Could not add issuer.");
+    // TODO: Process distinct possible errors here accordingly
+    registry
+        .add_issuer(issuer)
+        .map_err(|_e| UserError::InternalServerError)?;
 
-    HttpResponse::Ok().json(issuer_id)
+    Ok(HttpResponse::Ok().json(issuer_id))
 }
 
 #[derive(Deserialize)]
@@ -37,11 +70,13 @@ struct AddVerificationRequest {
 async fn add_verification(
     request: web::Json<AddVerificationRequest>,
     registry: web::Data<Mutex<VerifiableDataRegistry>>,
-) -> HttpResponse {
-    let mut registry = registry.lock().expect("Could not lock registry.");
-    let issuer_id = URL::new(&request.issuer_id).expect("Invalid issuer ID.");
+) -> Result<HttpResponse, UserError> {
+    let mut registry = registry
+        .lock()
+        .map_err(|_e| UserError::InternalServerError)?;
+    let issuer_id = URL::new(&request.issuer_id).map_err(|_e| UserError::BadRequest)?;
     let verification_method_id =
-        URL::new(&request.verification_method_id).expect("Invalid verification method ID.");
+        URL::new(&request.verification_method_id).map_err(|_e| UserError::BadRequest)?;
     let verification_method = VerificationMethod::new(
         verification_method_id.clone(),
         request.type_.clone(),
@@ -49,11 +84,12 @@ async fn add_verification(
         request.public_key_multibase.clone(),
     );
 
+    // TODO: Process distinct possible errors here accordingly
     registry
         .add_verification_method(&issuer_id, verification_method)
-        .expect("Could not add verification method.");
+        .map_err(|_e| UserError::InternalServerError)?;
 
-    HttpResponse::Ok().json(verification_method_id)
+    Ok(HttpResponse::Ok().json(verification_method_id))
 }
 
 #[get("/add_schema")]
