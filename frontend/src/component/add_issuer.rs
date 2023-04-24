5881fc20cb3255a8b1a179af2af81ecb9409d8f2
AndrewCLu <andrewclu98@gmail.com>
2023-04-24 19:32:38 -0400

    create component for user to make a new issuer

---
+++ frontend/src/component/add_issuer.rs
@@ -0,0 +1,56 @@
+use serde_json::json;
+use wasm_bindgen::JsCast;
+use web_sys::{EventTarget, HtmlInputElement};
+use yew::{platform::spawn_local, prelude::*};
+
+#[function_component]
+pub fn AddIssuer() -> Html {
+    let name = use_state(|| "".to_string());
+
+    let handle_input = {
+        let name = name.clone();
+        Callback::from(move |e: InputEvent| {
+            let target: Option<EventTarget> = e.target();
+            let input = target.and_then(|t| t.dyn_into::<HtmlInputElement>().ok());
+
+            if let Some(input) = input {
+                name.set(input.value());
+            }
+        })
+    };
+
+    let on_submit = {
+        let name = name.clone();
+        Callback::from(move |e: SubmitEvent| {
+            e.prevent_default();
+            let name = name.clone();
+            let request_data = json!({
+                "id": "https://example.com/issuer/1",
+                "name": "Issuer Name",
+            });
+            spawn_local(async move {
+                let client = reqwest::Client::new();
+                let res = client
+                    .post("http://127.0.0.1:8000/issuer/add_issuer")
+                    .json(&request_data)
+                    .send()
+                    .await
+                    .unwrap();
+                println!("Response: {:?}", res);
+                name.set("".to_string());
+            });
+        })
+    };
+
+    html! {
+        <form onsubmit={on_submit}>
+            <input
+                type="text"
+                placeholder="Issuer Name"
+                value={(*name).clone()}
+                oninput={handle_input}
+            />
+            <button type="submit">{"Submit"}</button>
+        </form>
+    }
+}
