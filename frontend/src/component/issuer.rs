4bf14c33f2bb817f517425b20290d3c44bf8e4cb
AndrewCLu <andrewclu98@gmail.com>
2023-04-25 02:49:07 -0400

    create hook to fetch issuers

--- frontend/src/component/issuer.rs
+++ frontend/src/component/issuer.rs
@@ -2,40 +2,56 @@ use std::rc::Rc;
 use vc_core::Issuer;
 use yew::{platform::spawn_local, prelude::*};
 
-async fn fetch_issuers() -> Result<Vec<Issuer>, reqwest::Error> {
+async fn get_all_issuers() -> Result<Vec<Issuer>, reqwest::Error> {
     let url = "http://localhost:8000/issuer/get_all_issuers";
     let resp = reqwest::get(url).await?;
     let issuers: Vec<Issuer> = resp.json().await?;
     Ok(issuers)
 }
 
-#[function_component(IssuerList)]
-pub fn issuer_list() -> Html {
-    let issuers = use_state(Vec::<Issuer>::new);
-    let loading = use_state(|| true);
+#[hook]
+pub fn use_issuers() -> (Rc<Vec<Issuer>>, Rc<bool>, Rc<Callback<()>>) {
+    let issuers = use_state(|| Rc::new(Vec::<Issuer>::new()));
+    let loading = use_state(|| Rc::new(true));
 
     let (issuers_clone, loading_clone) = (issuers.clone(), loading.clone());
-    use_effect_with_deps(
-        |()| {
+    let fetch_issuers = {
+        Rc::new(Callback::from(move |_| {
+            let issuers = issuers_clone.clone();
+            let loading = loading_clone.clone();
             let future = async move {
-                match fetch_issuers().await {
+                match get_all_issuers().await {
                     Ok(new_issuers) => {
-                        issuers_clone.set(new_issuers);
-                        loading_clone.set(false);
+                        issuers.set(Rc::new(new_issuers));
+                        loading.set(Rc::new(false));
                     }
                     Err(_) => {
                         eprintln!("Failed to fetch issuers.");
-                        loading_clone.set(false);
+                        loading.set(Rc::new(false));
                     }
                 }
             };
-
             spawn_local(future);
+        }))
+    };
+
+    let fetch_issuers_clone = fetch_issuers.clone();
+    use_effect_with_deps(
+        move |_| {
+            fetch_issuers_clone.emit(());
             || ()
         },
         (),
     );
 
+    let issuers = (*issuers).clone();
+    let loading = (*loading).clone();
+    (issuers, loading, fetch_issuers)
+}
+
+#[function_component(IssuerList)]
+pub fn issuer_list() -> Html {
+    let (issuers, loading, _) = use_issuers();
     let issuer_list = issuers
         .iter()
         .map(|issuer| {
